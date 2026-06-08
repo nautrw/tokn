1be2fc933a4a51998a3d2a6284dee33968cb12af
nautrw <nautrw@proton.me>
2026-06-08 12:49:56 -0400

    encode salt in base64 to correctly split the outputs

--- tokn/encryption.py
+++ tokn/encryption.py
@@ -4,32 +4,39 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
+
 def gen_password_key(password: bytes, salt: bytes) -> bytes:
     kdf = Argon2id(
-        salt=salt,
-        length=32,
-        iterations=1,
-        lanes=4,
-        memory_cost=2**21 # 2.09 mb
+        salt=salt, length=32, iterations=1, lanes=4, memory_cost=2**21  # 2.09 gb
     )
-    
+
     key = kdf.derive(password)
     return base64.urlsafe_b64encode(key)
+
 
 def encrypt_to_file(filename: str, text: str, salt: bytes, key: bytes) -> None:
     fernet = Fernet(key)
     token = fernet.encrypt(text.encode())
-    
-    with open(filename, 'w') as f:
-        f.write(f"{salt}\n{token}")
 
-def decrypt_from_file(filename: str, key: bytes):
+    with open(filename, "wb") as f:
+        f.write(base64.b64encode(salt))
+        f.write(b"\n")
+        f.write(token)
+
+
+def get_salt_from_file(filename: str):
+    with open(filename, "r") as f:
+        salt, _ = f.read().splitlines()
+
+    return base64.b64decode(salt)
+
+
+def decrypt_keys_from_file(filename: str, key: bytes):
     fernet = Fernet(key)
-    
-    with open(filename, 'r') as f:
-        salt, encrypted = f.read().splitlines()
-        print(encrypted)
-        
-    # decrypted = fernet.decrypt(encrypted)
-    
-    # return salt, decrypted
+
+    with open(filename, "r") as f:
+        _, encrypted = f.read().splitlines()
+
+    decrypted = fernet.decrypt(encrypted)
+
+    return decrypted.decode()
