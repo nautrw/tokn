39b1ce1421db2c65d36087f2ad7a4e13c473c0a0
nautrw <nautrw@proton.me>
2026-06-09 11:23:36 -0400

    add init command

--- tokn/cli.py
+++ tokn/cli.py
@@ -4,11 +4,14 @@ import pyotp
 import tokn.encryption as encryption
 import json
 from math import floor
+import os
 
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    if not os.path.isfile("keys") and not ctx.invoked_subcommand == "init":
+        exit("Keys file not initialized. Please run `tokn init`.")
 
 
 @cli.command()
@@ -51,3 +54,24 @@ def get(name: str):
     click.echo(f"Code: {totp}")
     click.echo(f"{floor(time_remaining)} seconds left")
     click.echo(f"Next code: {next_code}")
+
+
+@cli.command()
+def init():
+    """Set up a new keys file."""
+    if os.path.isfile("keys"):
+        exit("There is already an existing keys file.")
+
+    passwd_input1 = click.prompt("Please create a new password", hide_input=True)
+    passwd_input2 = click.prompt("Please enter the password again", hide_input=True)
+
+    if not passwd_input1 == passwd_input2:
+        exit("Passwords must be the same. Please try again.")
+
+    random_salt = os.urandom(16)
+    key = encryption.gen_password_key(passwd_input1.encode(), random_salt)
+
+    open("keys", "x")
+    encryption.encrypt_to_file("keys", "{}", random_salt, key)
+
+    click.echo("Successfully created new keys file.")
