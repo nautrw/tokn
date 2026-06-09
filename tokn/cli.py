ea15fdad16e9b5ea1ec6ad63c6e6a7d0bdd983af
nautrw <nautrw@proton.me>
2026-06-09 09:18:58 -0400

    add command to add keys

--- tokn/cli.py
+++ tokn/cli.py
@@ -1,9 +1,31 @@
 import click
 import tokn.otp as otp
 import pyotp
+import tokn.encryption as encryption
+import json
 
+@click.group()
+def cli():
+    pass
 
-@click.command()
-def main():
-    key = pyotp.random_base32()
-    print(f"Generated totp: {otp.generate_totp(key)}")
+@cli.command()
+@click.argument('name', required=True)
+def add(name: str):
+    """Associate a secret key to the service NAME.
+    
+    NAME is the name of the service.
+    """
+    salt, encrypted = encryption.get_file_info("keys")
+    password = click.prompt("Enter your password", hide_input=True).encode()
+    key = encryption.gen_password_key(password, salt)
+    decrypted = encryption.decrypt(encrypted, key)
+    
+    keys_dict = json.loads(decrypted)
+    
+    secret_key = click.prompt("Enter the secret key", hide_input=True)
+
+    keys_dict[name] = secret_key
+    
+    encryption.encrypt_to_file("keys", json.dumps(keys_dict), salt, key)
+    
+    click.echo(f"Successfully added key to {name}")
