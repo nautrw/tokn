c7363652de9dd0c960d72dd1639677a509dc90f1
nautrw <nautrw@proton.me>
2026-06-11 21:53:18 -0400

    pass in add group command

---
+++ tokn/commands/add.py
@@ -0,0 +1,62 @@
+import json
+from ast import parse
+
+import click
+import pyotp
+from cryptography.fernet import InvalidToken
+from platformdirs import PlatformDirs
+
+import tokn.encryption as encryption
+import tokn.otp as otp
+from tokn.qr import read_qr_code
+
+dirs = PlatformDirs("tokn", "nautrw", ensure_exists=True)
+KEYS_FILE = dirs.user_data_dir + "/keys"
+
+@click.group()
+@click.pass_context
+def add(ctx):
+    pass
+
+@add.command()
+def qr():
+    password = click.prompt("Enter your password", hide_input=True).encode()
+
+    try:                                                                     
+        keys_dict = encryption.get_keys_with_password(KEYS_FILE, password)
+    except InvalidToken:
+        raise click.ClickException("Incorrect password.")
+    
+    qr_path = click.prompt("Enter the path of the QR code image")
+    
+    try:
+        code = read_qr_code(qr_path)
+    except ValueError:
+        raise click.ClickException(
+            "Could not extract QR code from image. "
+            "Please ensure the image is valid."
+        )
+    
+    try:
+        parsed_uri = pyotp.parse_uri(code)
+    except ValueError:
+        raise click.ClickException("Invalid QR code.")
+    
+    if not otp.is_valid_secret(parsed_uri.secret):
+        raise click.ClickException("Invalid secret key.")
+
+    click.echo(f"Issuer: {parsed_uri.issuer}")
+    click.echo(f"Label: {parsed_uri.name}")
+    click.confirm("Are you sure you want to add this key?", abort=True)
+
+    keys_dict.append({
+        "issuer": parsed_uri.issuer,
+        "label": parsed_uri.name,
+        "secret": parsed_uri.secret
+    })
+    
+    salt = encryption.get_file_info(KEYS_FILE)[0]
+    key = encryption.gen_password_key(password, salt)
+    encryption.encrypt_to_file(KEYS_FILE, json.dumps(keys_dict), salt, key)
+    
+    click.echo(f"Successfully added key under issuer {parsed_uri.issuer}.")
