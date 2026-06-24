e362c08d3acf9d0e370f5cb036f5f67d77ee4d4c
nautrw <nautrw@proton.me>
2026-06-24 13:24:55 -0400

    remove unused aliases

--- tokn/cli.py
+++ tokn/cli.py
@@ -1,11 +1,10 @@
 import click
-from click_aliases import ClickAliasedGroup
 from tokn.commands.write_commands import add, remove, change_password
 from tokn.commands.getters import get, list, vault
 from tokn.qr import read_qr_code
 
 
-@click.group(cls=ClickAliasedGroup)
+@click.group()
 @click.pass_context
 def cli(ctx: click.core.Context):
     pass
