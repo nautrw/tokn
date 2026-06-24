1f7596a19a3a1368fc57d066ca90c592193466ea
nautrw <nautrw@proton.me>
2026-06-24 13:22:59 -0400

    remove unused imports in cli.py

--- tokn/cli.py
+++ tokn/cli.py
@@ -1,16 +1,5 @@
-import json
-import os
-from hashlib import new
-from math import floor
-
 import click
-import pyotp
 from click_aliases import ClickAliasedGroup
-from cryptography.fernet import InvalidToken
-
-import tokn.encryption as encryption
-from tokn.encryption import KEYS_FILE
-import tokn.otp as otp
 from tokn.commands.write_commands import add, remove, change_password
 from tokn.commands.getters import get, list, vault
 from tokn.qr import read_qr_code
